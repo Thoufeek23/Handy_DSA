e0c8589b1b8e34e8623b97a325f2df769d05fd00
Thoufeek23 <mali21@binghamton.edu>
2025-01-17 22:04:57 -0500

    Updated

---
+++ heap.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void heapify(vector<int> &arr, int n, int i)
+{
+    int large = i;
+    int l = (2 * i) + 1;
+    int r = (2 * i) + 2;
+    if (l < n && arr[l] > arr[large])
+    {
+        large = l;
+    }
+    if (r < n && arr[r] > arr[large])
+    {
+        large = r;
+    }
+    if (large != i)
+    {
+        swap(arr[i], arr[large]);
+        heapify(arr, n, large);
+    }
+}
+void heapsort(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+void disp(vector<int> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    vector<int> arr = {4, 3, 5, 2, 1};
+    cout << "Array: " << endl;
+    disp(arr);
+    int n = arr.size();
+    for (int i = (n / 2) - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+    cout << "Heapified array: " << endl;
+    disp(arr);
+    heapsort(arr);
+    cout << "Sorted array: " << endl;
+    disp(arr);
+}
